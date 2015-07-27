4b12e10120c6789680bf2cfdc18771e1e611f993
Victor Ryzhenkin <vryzhenkin@mirantis.com>
2015-07-27 21:35:37 +0300

    Fixed paths and using global variables for paths

--- tools/init_env_variables.sh
+++ tools/init_env_variables.sh
@@ -10,3 +10,11 @@ PYTHON_VERSION="${PYTHON_VERSION:-2.7.9}"
 PYTHON_LOCATION="https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz"
 
 PIP_LOCATION="https://raw.github.com/pypa/pip/master/contrib/get-pip.py"
+
+message() {
+    printf "\e[33m%s\e[0m\n" "${1}"
+}
+
+error() {
+    printf "\e[31mError: %s\e[0m\n" "${*}" >&2
+}
